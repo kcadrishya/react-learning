1fdb720de96dc50684694812698480071699c94d
kcadrishya <kcupendra@hotmail.com>
2019-02-04 21:09:45 +1100

    created basic routing structure

--- router-lifecycles/src/index.js
+++ router-lifecycles/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//components
+import Posts from './components/posts';
+import Profiles from './components/profiles';
+import Home from './components/home';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = ()=>{
+    return(
+        <BrowserRouter>
+            <div>
+                <header>
+                    Header
+                </header>
+                <Route path="/" exact component={Home}/>
+                <Route path="/posts" component={Posts}/>
+                <Route path="/profiles" component={Profiles}/>
+                <Route path="/home" component={Home}/>
+            </div>
+        </BrowserRouter>
+    )   
+}
+
+ReactDOM.render(
+    <App/>, document.querySelector('#root')
+);
