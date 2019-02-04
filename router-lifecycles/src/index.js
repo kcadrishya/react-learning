5c4880a79f15e84c47385daf1a00d17e58e74e53
kcadrishya <kcupendra@hotmail.com>
2019-02-04 21:59:57 +1100

    route parameters and urls

--- router-lifecycles/src/index.js
+++ router-lifecycles/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
+//alternative to Browser Router are HashRouter and MemoryRouter
+//link can be replaced by NavLink which has option to add activeStyle and activeClassName as a property of link
+
 // components
 import Posts from './components/posts';
 import Profiles from './components/profiles';
 import Home from './components/home';
+import PostItem from './components/post_item';
 
 // myawesomeapp.com/posts
 // myawesomeapp.com/profile/posts
@@ -22,7 +26,8 @@ const App = ()=>{
                     <hr/>
                 </header>
                 <Route path="/" exact component={Home}/>
-                <Route path="/posts" component={Posts}/>
+                <Route path="/posts" exact component={Posts}/>
+                <Route path="/posts/:id/:username" component={PostItem}/>
                 <Route path="/profiles" component={Profiles}/>
                 <Route path="/home" component={Home}/>
             </div>
